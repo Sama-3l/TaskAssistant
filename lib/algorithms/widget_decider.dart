e90e4cac6568a636efeb54f3692283f132a01ad4
Sama-3l <raghvendramishra2002@gmail.com>
2024-12-08 22:53:42 +0530

    Sign out fix

--- lib/algorithms/widget_decider.dart
+++ lib/algorithms/widget_decider.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:taskassistant/presentation/screens/onboarding.dart';
 
 class WidgetDecider {
   void showOptions(BuildContext context) {
@@ -14,7 +15,7 @@ class WidgetDecider {
               child: const Text('Sign Out'),
               onPressed: () async {
                 await FirebaseAuth.instance.signOut();
-                Navigator.of(context).pop();
+                Navigator.of(context).pushAndRemoveUntil(CupertinoPageRoute(builder: (context) => OnBoarding()), (Route) => false);
               },
             ),
           ],
